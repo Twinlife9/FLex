6302dece2c56e140030140bf2bd0bbe0613b41cd
Camisul <Camisul@camisul.ml>
2019-03-20 12:19:20 +0300

    Added username input logic

--- Game/index.js
+++ Game/index.js
@@ -1,1 +1,13 @@
-console.log('Ayy fledx');
+const form = document.querySelector('form');
+const panel = document.querySelector('.panel'); 
+const field_uname = document.querySelector('#unamefield');
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  const fd = new FormData(form)
+  const uname = fd.get('username');
+  if (uname != '') {
+    console.log('succes');
+    field_uname.innerText = uname;
+    panel.style.display = 'none';
+  }
+}
