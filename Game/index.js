6aad6072dab5366716a2a28d6289c1f97cd393e5
Camisul <Camisul@camisul.ml>
2019-03-20 13:19:12 +0300

    Menu state hadnling stugg

--- Game/index.js
+++ Game/index.js
@@ -1,13 +1,36 @@
-const form = document.querySelector('form');
-const panel = document.querySelector('.panel'); 
 const field_uname = document.querySelector('#unamefield');
-form.onsubmit = (e)=>{
-  e.preventDefault();
-  const fd = new FormData(form)
-  const uname = fd.get('username');
-  if (uname != '') {
-    console.log('succes');
-    field_uname.innerText = uname;
-    panel.style.display = 'none';
+const form = document.querySelector('form');
+const start = document.querySelector('.screen-start');
+const start_button = document.querySelector('#start_btn');
+
+function proceedStart(state) {
+  switch (state) {
+    case 0:
+      {
+        form.onsubmit = e => {
+          e.preventDefault();
+          const fd = new FormData(form);
+          const uname = fd.get('username');
+          if (uname != '') {
+            console.log('succes');
+            field_uname.innerText = uname;
+            start.style.display = 'none';
+            proceedStart(1);
+          }
+        };
+      }
+      break;
+    case 1:
+      {
+        start_button.onclick = e => {
+          const ranking = document.querySelector('.screen-ranking');
+          ranking.style.display = 'none';
+          proceedStart(2);
+        };
+      }
+      break;
+    default:
+      break;
   }
 }
+proceedStart(0);
