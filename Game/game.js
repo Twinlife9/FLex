6d9fa45d9082f69a5af5784edf3a1ea465c4d606
Camisul <Camisul@camisul.ml>
2019-03-22 12:48:56 +0300

    more Movement work, pause and some attack anims

--- Game/game.js
+++ Game/game.js
@@ -1,20 +1,50 @@
 /////
 const DEBUG = true;
 
-let player = 0;
+let isPause = false;
+let player = {
+  hp: 100,
+  mp: 100,
+  elem: 0,
+};
+
 let game = 0;
 function GameStart() {
   game = document.querySelector('.screen-game');
-  player = document.createElement('div');
-  player.className = 'player-idle';
-  player.style.top = '100px';
-  player.style.left = '100px';
-  game.appendChild(player);
+  player.elem = document.createElement('div');
+  player.elem.className = 'player-idle';
+  player.elem.style.top = '100px';
+  player.elem.style.left = '100px';
+  game.appendChild(player.elem);
   SetupMovement();
   StartTimer();
   setTimeout(() => {
     setInterval(GameLoop, 1000 / 60);
   }, 20);
+}
+
+function Pause() {
+  let classStates = ['idle', 'mv', 'sprint'];
+  if (!isPause) {
+    //For every animating object ....
+    {
+      classStates.forEach(el => {
+        let target = el + '-static';
+        player.elem.className = player.className.replace(el, target);
+      });
+    }
+    isPause = true;
+  } else {
+    //For every animating object ....
+    {
+      classStates.forEach(el => {
+        let target = el + '-static';
+        player.elem.className = player.className.replace(target, el);
+      });
+    }
+
+    isPause = false;
+  }
 }
 
 function getTime(start) {
@@ -28,72 +58,105 @@ function getTime(start) {
 let iters = 0;
 function StartTimer() {
   setInterval(() => {
+    if (isPause) {
+      return;
+    }
     let timer = document.querySelector('.timer');
     let time = getTime(iters);
     timer.innerHTML = `Time: ${time}`;
     iters++;
+    //HP & MP REGEN
+    player.mp = Math.min(Math.max(player.mp + 5, 0), 100);
+    player.hp = Math.min(Math.max(player.hp + 2, 0), 100);
   }, 1000);
 }
 
 let xvel = 0;
 let isShfit = false;
+let attackCnt = 0;
 function SetupMovement() {
   document.onkeydown = e => {
     console.log(e);
     isShfit = e.shiftKey;
     switch (e.keyCode) {
+      case 27:
+        Pause();
+        break;
+      //MOVEMENT
       case 37:
         xvel = -3;
-        break;
-      case 38:
-        player.style.top = player.offsetTop - 3 + 'px';
         break;
       case 39:
         xvel = 3;
         break;
+      //Attacks
+      case 49:
+        if (attackCnt < 1) {
+          Attack1(player);
+        } else {
+          Attack1loop(player);
+        }
+        attackCnt++;
+        break;
+
+      case 50:
+        Block(player);
+        break;
+
       default:
         break;
     }
   };
   document.onkeyup = e => {
-    player.className = player.className
-      .replace('sprint', 'mv')
-      .replace('mv', 'idle');
-      xvel = 0;
+    player.elem.className =
+      'player-idle ' + player.elem.className.split(' ')[1];
+    xvel = 0;
+    attackCnt = 0;
   };
 }
 
 let backgroundPos = 0;
 
 function GameLoop() {
+  //Pause thingy
+  if (isPause) {
+    return;
+  }
+
+  //Keeping track of hp and mp values
+  updateMP(player.mp);
+  updateHP(player.hp);
+
   ///
   ///PLAYER MOVEMENT
   ///
 
   if (xvel != 0) {
     //Win in the end
-    if (player.offsetLeft >= (screen.width - player.clientWidth * 2)) {
-    console.log('WIN', 'TIME', getTime(iters));
-    return;
+    if (player.elem.offsetLeft >= screen.width - player.elem.clientWidth * 2) {
+      // console.log('WIN', 'TIME', getTime(iters));
+      return;
     }
     //left border
-    if (player.offsetLeft > 0 || xvel > 0) {
+    if (player.elem.offsetLeft > 0 || xvel > 0) {
       let speed_mp = DEBUG ? 10 : 2;
       //if moved more than half of the screen wdith
       //scroll the backgound
       if (
-         player.offsetLeft < (screen.width / 2 - player.clientWidth / 2)   
-         ||  backgroundPos >= 6500 || backgroundPos == 1  
+        player.elem.offsetLeft < screen.width / 2 - player.elem.clientWidth / 2 ||
+        backgroundPos >= 6500 ||
+        backgroundPos == 1
       ) {
-        console.log('WALK OR SPRINT',player.offsetLeft, backgroundPos );
+        console.log('WALK OR SPRINT', player.elem.offsetLeft, backgroundPos);
         if (backgroundPos == 1) {
           backgroundPos = 0;
         }
         //Sprint stuff...
         if (isShfit) {
-          player.style.left = player.offsetLeft + xvel * speed_mp + 'px';
+          player.elem.style.left =
+            player.elem.offsetLeft + xvel * speed_mp + 'px';
         } else {
-          player.style.left = player.offsetLeft + xvel + 'px';
+          player.elem.style.left = player.elem.offsetLeft + xvel + 'px';
         }
       } else {
         //Scroll da backround
@@ -105,9 +168,10 @@ function GameLoop() {
       }
 
       //animations
-      player.className = 'player-mv ' + (xvel > 0 ? 'look-right' : 'look-left');
+      player.elem.className =
+        'player-mv ' + (xvel > 0 ? 'look-right' : 'look-left');
       if (isShfit) {
-        player.className = player.className.replace('mv', 'sprint');
+        player.elem.className = player.elem.className.replace('mv', 'sprint');
       }
     }
   }
--- Game/player.js
+++ Game/player.js
@@ -1,3 +1,42 @@
-function moveLeft(){
-  
+function updateBar(polygon, textValue, pp) {
+  textValue.innerHTML = pp;
+  if (polygon.className == 'panel-mp') {
+    textValue.style.left = `${pp - 20}px`;
+  } else {
+    textValue.style.left = `${pp}px`;
+  }
+  polygon.style.clipPath = `polygon(0% 0%, ${pp}% 0%, ${pp -
+    10}% 100%, 0% 100%)`;
+  if (polygon.className == 'panel-mp' && pp < 40) {
+    textValue.style.display = 'none';
+  } else if (pp < 20) {
+    textValue.style.display = 'none';
+  } else {
+    textValue.style.display = 'block';
+  }
+}
+
+function updateHP(hp) {
+  let polygon = document.querySelector('.panel-xp');
+  let textValue = document.querySelector('.panel-xp > div > span');
+  updateBar(polygon, textValue, hp);
+}
+
+function updateMP(mp) {
+  let polygon = document.querySelector('.panel-mp');
+  let textValue = document.querySelector('.panel-mp > div > span');
+  updateBar(polygon, textValue, mp);
+}
+
+function Attack1(player) {
+  player.elem.className = 'player-attack-one-one';
+}
+
+
+function Attack1loop(player) {
+  player.elem.className = 'player-attack-one-loop';
+}
+
+function Block(player) {
+  player.elem.className = 'player-block';
 }
